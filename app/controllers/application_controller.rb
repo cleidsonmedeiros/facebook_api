88abdbb1e9bb981c0164b98ab6afd4775f05a796
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-11-13 22:27:17 -0300

    disable spoofing protection for JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
         include DeviseTokenAuth::Concerns::SetUserByToken
-    #before_action :authenticate_user!
-    protect_from_forgery prepend: true
+    protect_from_forgery unless: -> { request.format.json? }
 end
