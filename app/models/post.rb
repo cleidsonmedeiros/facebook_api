b39c2bef431d0cc2e89ed27fef35779ddf03c95d
cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2023-12-13 03:19:49 -0300

    associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_one_attached :image
+  has_many :likes
   has_many :comments
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   extend Devise::Models  
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable            
   include DeviseTokenAuth::Concerns::User
-
 end
